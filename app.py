bbec3ad315c1f5695e4cea334a4775eea01cc123
sd2001 <im.swarnabha2001@gmail.com>
2020-09-09 02:01:07 +0530

    Testing requests using mutiple form values

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        val1=request.form.get('gender')
+        val2=request.form.get('gender2')
+        val3=int(val1)+int(val2)
+        return str(val3)
+    return render_template('htmlform.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
