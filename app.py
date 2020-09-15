aa340c85739c35b8bfb8b545bb509b0a03d900f5
sd2001 <im.swarnabha2001@gmail.com>
2020-09-15 14:33:33 +0530

    Adding sessions for login and logout

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask,render_template,request,flash,redirect,url_for
+from flask import Flask,render_template,request,flash,redirect,url_for,g,session
 import random
 import torch
 import json
 from model import NeuralNet
 from basics import bag_of_words, tokenize, stem
 from nearby import hospital_search,pharmacy_search
+import pymongo
+from pymongo import MongoClient
+from flask_login import login_user,current_user
+import time
+from datetime import date
 
 app=Flask(__name__)
-app.secret_key = 'hellouserapi'
+app.secret_key = 'hellomic'
+
+@app.before_request
+def before_request():
+    g.user=None
+    det=[]
+    if 'user' in session:
+        g.user=session['user']
 
 @app.route("/")
 def login():    
+    if g.user:
+        return redirect(url_for('test'))
     return render_template('login.html')
 
 @app.route("/register")
@@ -66,12 +80,18 @@ def doc_vc():
 
 @app.route("/tests")
 def test():
-    return render_template('patient_home.html')
+    if g.user:
+        return render_template('patient_home.html')
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
 
 
 @app.route("/tests/chatbot")
 def chat():
-     return render_template("Chatbot_temp.html")
+    if g.user:
+        return render_template("Chatbot_temp.html")
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
 
 @app.route("/tests/chatbot_get")
 def chatbot():
@@ -115,53 +135,61 @@ def chatbot():
 
 @app.route("/tests/self_assess",methods=['GET','POST'])
 def self_assess():
-    if request.method=='POST':
-        score=50
-        fear=request.form.get('fear')
-        eat=request.form.get('eat')
-        confidence=request.form.get('confidence')
-        future=request.form.get('future')
-        relation=request.form.get('relation')
-        activity=request.form.get('activity')
-        score=score+int(fear)+int(eat)+int(confidence)+int(future)+int(relation)+int(activity)
-        mssg=""
-        if score>50 and score<100:
-            mssg="Great, make your days even better!"
-        elif score>100:
-            mssg="Awesome, thats the motivation we need!"
-        elif score<50 and score>0:
-            mssg="Try to stay motiavted always. We would recommend you to consult Dr A."            
-        elif score<0:
-            mssg="Its highly important for you to consult Dr A, before something big happens."
-        
-        flash(mssg)
+    if g.user:
+        if request.method=='POST':
+            score=50
+            fear=request.form.get('fear')
+            eat=request.form.get('eat')
+            confidence=request.form.get('confidence')
+            future=request.form.get('future')
+            relation=request.form.get('relation')
+            activity=request.form.get('activity')
+            score=score+int(fear)+int(eat)+int(confidence)+int(future)+int(relation)+int(activity)
+            mssg=""
+            if score>50 and score<100:
+                mssg="Great, make your days even better!"
+            elif score>100:
+                mssg="Awesome, thats the motivation we need!"
+            elif score<50 and score>0:
+                mssg="Try to stay motiavted always. We would recommend you to consult Dr A."            
+            elif score<0:
+                mssg="Its highly important for you to consult Dr A, before something big happens."
+            
+            flash(mssg)
+            return render_template('self-assessment2.html')
         return render_template('self-assessment2.html')
-    return render_template('self-assessment2.html')
+    
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
 
 @app.route("/tests/inkblot",methods=['GET','POST'])
 def inkblot():
-    if request.method=='POST':
-        score=50
-        fear=request.form.get('1')
-        eat=request.form.get('2')
-        confidence=request.form.get('3')
-        future=request.form.get('4')
-        relation=request.form.get('5')
-        
-        score=score+int(fear)+int(eat)+int(confidence)+int(future)+int(relation)
-        mssg=""
-        if score>=50 and score<100:
-            mssg="Great, make your days even better!"
-        elif score>=100:
-            mssg="Awesome, thats the motivation we need!"
-        elif score<50 and score>0:
-            mssg="Try to stay motivated always. We would recommend you to consult a doctor."            
-        elif score<=0:
-            mssg="Its highly important for you to consult a doctor, before something big happens."
-        
-        flash(mssg)
+    if g.user:
+        if request.method=='POST':
+            score=50
+            fear=request.form.get('1')
+            eat=request.form.get('2')
+            confidence=request.form.get('3')
+            future=request.form.get('4')
+            relation=request.form.get('5')
+            
+            score=score+int(fear)+int(eat)+int(confidence)+int(future)+int(relation)
+            mssg=""
+            if score>=50 and score<100:
+                mssg="Great, make your days even better!"
+            elif score>=100:
+                mssg="Awesome, thats the motivation we need!"
+            elif score<50 and score>0:
+                mssg="Try to stay motivated always. We would recommend you to consult a doctor."            
+            elif score<=0:
+                mssg="Its highly important for you to consult a doctor, before something big happens."
+            
+            flash(mssg)
+            return render_template('inkblot.html')
         return render_template('inkblot.html')
-    return render_template('inkblot.html')
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
+
 
 @app.route("/doctor_list")
 def doctor_list():
@@ -170,54 +198,80 @@ def doctor_list():
 
 @app.route("/video_patient/<string:name>")
 def v_p(name):
-    return render_template('video_chat.html',name=name)
+    if g.user:
+        return render_template('video_chat.html',name=name)
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
 
 
 @app.route("/shop",methods=['GET','POST'])
 def shop_search():
-    items=["A Tabs","B Tabs","C Tabs"]
-    qnty=[10,18,5]
-    price=[10,15,18]
-    res=False
-    if request.method=='POST':       
-        search=request.form.get('search')
-        for i in range(0,len(items)):
-            if search.lower()==items[i].lower():
-                res=True
-                q=qnty[i]
-                p=price[i]
-                return render_template('shop2.html',res=res,q=q,p=p,item=search)
+    if g.user:
+        items=["A Tabs","B Tabs","C Tabs"]
+        qnty=[10,18,5]
+        price=[10,15,18]
+        res=False
+        if request.method=='POST':       
+            search=request.form.get('search')
+            for i in range(0,len(items)):
+                if search.lower()==items[i].lower():
+                    res=True
+                    q=qnty[i]
+                    p=price[i]
+                    return render_template('shop2.html',res=res,q=q,p=p,item=search)
+            
+            return render_template('shop2.html',res=res)
         
-        return render_template('shop2.html',res=res)
-    
-    return render_template('shop.html')
+        return render_template('shop.html')
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
+
 
 @app.route("/shop/confirmation")
 def confirm():
-    return render_template('payment_ack.html')      
+    if g.user:
+        return render_template('payment_ack.html')  
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
+
+
+@app.route("/prescription")
+def pres():
+    if g.user:
+        return render_template('prescription.html')  
+    flash("Please login before continuing")
+    return render_template('login.html',user='g')   
 
 @app.route("/logout")
 def logoute():
+    if g.user:
+        session.pop('user', None)
+        return render_template('login.html')
+    flash("Please login before continuing")
     return render_template('login.html')  
 
 @app.route("/nearby",methods=['GET','POST'])
 def neerby():
-    res=False
-    if request.method=='POST':
-        choice=request.form.get('choice')
-        locality=request.form.get('locality')
-        
-        if choice=='Hospital':
-            names=hospital_search(locality)
-            res=True
+    if g.user:
+        res=False
+        if request.method=='POST':
+            choice=request.form.get('choice')
+            locality=request.form.get('locality')
             
-        else:
-            names=pharmacy_search(locality)
-            res=True
+            if choice=='Hospital':
+                names=hospital_search(locality)
+                res=True
+                
+            else:
+                names=pharmacy_search(locality)
+                res=True
+                
+            return render_template('hospital.html',res=res,names=names)        
             
-        return render_template('hospital.html',res=res,names=names)        
-        
-    return render_template('hospital.html',res=res)
+        return render_template('hospital.html',res=res)
+    
+    flash("Please login before continuing")
+    return render_template('login.html',user='g') 
         
     
 if __name__=="__main__":
